177ad101d90dd79cc1f8a7753d06e9e59b19684c
francoromanazzi <francoromanazzi@hotmail.com>
2020-01-30 14:04:55 -0300

    Fix validators array alumnos

--- cursosCRUD/cursosValidators.js
+++ cursosCRUD/cursosValidators.js
@@ -5,11 +5,11 @@ const postValidators = [
     check('anioDictado').isInt().withMessage("anioDictado debe ser entero"),
     check('tema').isString().withMessage("tema debe ser string"),
     check('duracion').isNumeric().withMessage("duracion debe ser numerico"),
-    check('alumnos.nombre').isString().withMessage("alumnos.nombre debe ser string"),
-    check('alumnos.apellido').isString().withMessage("alumnos.apellido debe ser string"),
-    check('alumnos.dni').isString().withMessage("alumnos.dni debe ser string"),
-    check('alumnos.direccion').isString().withMessage("alumnos.direccion debe ser string"),
-    check('alumnos.nota').isNumeric().withMessage("alumnos.nota debe ser numerico"),
+    check('alumnos.*.nombre').isString().withMessage("alumnos.nombre debe ser string"),
+    check('alumnos.*.apellido').isString().withMessage("alumnos.apellido debe ser string"),
+    check('alumnos.*.dni').isString().withMessage("alumnos.dni debe ser string"),
+    check('alumnos.*.direccion').isString().withMessage("alumnos.direccion debe ser string"),
+    check('alumnos.*.nota').isNumeric().withMessage("alumnos.nota debe ser numerico"),
 ];
 
 const cursoExiste = (req, res, next) => {
