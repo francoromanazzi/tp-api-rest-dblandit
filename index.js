d5ac4c413ebce6112b01ba5ef427c5eceb60c7da
francoromanazzi <francoromanazzi@hotmail.com>
2020-02-24 17:24:01 -0300

    Agrego CORS al backend

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/cursos";
 
 // Middleware
 app.use(bodyParser.json());
+// CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 
 // Routes
 app.use('/api/v1/cursos', cursosRouter);
